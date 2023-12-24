3412578a4de04a97cf46dd677b6bd0ba2156aff0
Sayman Rifat <saymanrifat@gmail.com>
2023-12-24 18:39:34 +0600

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Sayman Rifat on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
